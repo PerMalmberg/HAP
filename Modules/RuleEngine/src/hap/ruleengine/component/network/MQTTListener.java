b6b56b4bf98bf34835952330a1a4614468ce58b9
Per Malmberg <per.malmberg@gmail.com>
2017-01-29 15:31:11 +0100

    Added outputs to MQTT component

--- Modules/RuleEngine/src/hap/ruleengine/component/network/MQTTListener.java
+++ Modules/RuleEngine/src/hap/ruleengine/component/network/MQTTListener.java
@@ -4,8 +4,8 @@ package hap.ruleengine.component.network;
 import hap.ruleengine.component.IPropertyDisplay;
 import hap.ruleengine.parts.Component;
 import hap.ruleengine.parts.composite.CompositeComponent;
-import hap.ruleengine.parts.property.BooleanProperty;
-import hap.ruleengine.parts.property.DoubleProperty;
+import hap.ruleengine.parts.output.BooleanOutput;
+import hap.ruleengine.parts.output.StringOutput;
 import hap.ruleengine.parts.property.IntProperty;
 import hap.ruleengine.parts.property.StringProperty;
 
@@ -21,15 +21,22 @@ public class MQTTListener extends Component
 	@Override
 	public void setup( CompositeComponent cc )
 	{
+		dataOut = new StringOutput( "Data", UUID.fromString( "3268b8b9-c544-4b0a-ac7c-fdeb64031d48" ), this );
+		isConnected = new BooleanOutput( "Connected", UUID.fromString( "46ac2e40-ccc0-4499-826b-1009bed9211b" ), this );
+
+		addOutput( dataOut );
+		addOutput( isConnected );
 
 	}
 
 	@Override
 	public void showProperties( IPropertyDisplay display )
 	{
-		display.show( new StringProperty( "Broker", "Broker", "localhost", "The broker to which a connection is made", this ) );
-		display.show( new IntProperty( "Port", "Port", 1883, 1, 65535,"The port of the broker to which a connection is made", this ) );
-		display.show( new DoubleProperty( "D", "DK", 55.0, 0, 100,"Test", this ) );
-		display.show( new BooleanProperty( "Box", "bb", true, "A boolean value", this ) );
+		display.show( new StringProperty( "Broker", "Broker", "localhost", 1, "The broker to which a connection is made", this ) );
+		display.show( new IntProperty( "Port", "Port", 1883, 1, 65535, "The port of the broker to which a connection is made", this ) );
+		display.show( new StringProperty( "Topic", "topic", "", 2, "The topic to subscribe to", this ) );
 	}
+
+	private StringOutput dataOut;
+	private BooleanOutput isConnected;
 }
