d60592e37ddc3cd221f23de1e4f25abf5353d21e
Per Malmberg <per.malmberg@gmail.com>
2017-02-19 16:21:00 +0100

    Simplified layout code.

--- Modules/RuleEngine/src/hap/ruleengine/editor/view/DebugView.kt
+++ Modules/RuleEngine/src/hap/ruleengine/editor/view/DebugView.kt
@@ -1,94 +1,91 @@
 package hap.ruleengine.editor.view
 
-import hap.ruleengine.component.placeholder.PlaceHolderComponent
 import hap.ruleengine.editor.viewmodel.event.SelectedComponentsChanged
-import hap.ruleengine.editor.viewmodel.parts.ComponentVM
 import hap.ruleengine.editor.viewmodel.parts.InputVM
 import hap.ruleengine.editor.viewmodel.parts.OutputVM
 import javafx.scene.paint.Color
 import javafx.scene.text.FontWeight
 import tornadofx.*
-import java.util.*
 
-class DebugView : View("") {
-    override val root = borderpane {}
+class DebugView : View("")
+{
+	override val root = borderpane {
+		top {
+			text("Debug Data") {
+				style {
+					fontSize = 14.px
+					fontWeight = FontWeight.BOLD
+				}
+			}
+		}
+		center {
+			vbox {
+				subscribe<SelectedComponentsChanged> {
+					this.replaceChildren {
+						if (it.selectedComponents.size == 1)
+						{
+							val single = it.selectedComponents.values.first()
 
-    init {
-        with(root) {
-            top {
-                text("Debug Data") {
-                    style {
-                        fontSize = 14.px
-                        fontWeight = FontWeight.BOLD
-                    }
-                }
-            }
-            center {
-                vbox {
-                    subscribe<SelectedComponentsChanged> {
-                        this.replaceChildren {
-                            if (it.selectedComponents.size == 1) {
-                                val single = it.selectedComponents.values.first()
+							hbox {
+								text("ID")
+								textfield(single.component.id.toString()) {
+									isEditable = false
+								}
+							}
 
-                                hbox {
-                                    text("ID")
-                                    textfield(single.component.id.toString()) {
-                                        isEditable = false
-                                    }
-                                }
+							tableview<OutputVM> {
+								items = single.outputs.observable()
+								column("Name", OutputVM::name).cellFormat {
+									graphic = textfield(itemProperty()) {
+										isEditable = false
+										style {
+											focusColor = Color.TRANSPARENT
+											backgroundColor += Color.TRANSPARENT
+										}
+									}
+								}
+								column("ID", OutputVM::id)
+										.cellFormat {
+											graphic = textfield(itemProperty(), UUIDConverter()) {
+												isEditable = false
+												style {
+													focusColor = Color.TRANSPARENT
+													backgroundColor += Color.TRANSPARENT
+												}
+											}
+										}
 
-                                tableview<OutputVM> {
-                                    items = single.outputs.observable()
-                                    column("Name", OutputVM::name).cellFormat {
-                                        graphic = textfield(itemProperty()) {
-                                            isEditable = false
-                                            style {
-                                                focusColor = Color.TRANSPARENT
-                                                backgroundColor += Color.TRANSPARENT
-                                            }
-                                        }
-                                    }
-                                    column("ID", OutputVM::id)
-                                            .cellFormat {
-                                                graphic = textfield(itemProperty(), UUIDConverter()) {
-                                                    isEditable = false
-                                                    style {
-                                                        focusColor = Color.TRANSPARENT
-                                                        backgroundColor += Color.TRANSPARENT
-                                                    }
-                                                }
-                                            }
+							}
 
-                                }
-
-                                tableview<InputVM> {
-                                    items = single.inputs.observable()
-                                    column("Name", InputVM::name).cellFormat {
-                                        graphic = textfield(itemProperty()) {
-                                            isEditable = false
-                                            style {
-                                                focusColor = Color.TRANSPARENT
-                                                backgroundColor += Color.TRANSPARENT
-                                            }
-                                        }
-                                    }
-                                    column("ID", InputVM::id).cellFormat {
-                                        graphic = textfield(itemProperty(), UUIDConverter()) {
-                                            isEditable = false
-                                            style {
-                                                focusColor = Color.TRANSPARENT
-                                                backgroundColor += Color.TRANSPARENT
-                                            }
-                                        }
-                                    }
-                                }
-                            } else {
-                                text("Select single component to see debug properties")
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
+							tableview<InputVM> {
+								items = single.inputs.observable()
+								column("Name", InputVM::name).cellFormat {
+									graphic = textfield(itemProperty()) {
+										isEditable = false
+										style {
+											focusColor = Color.TRANSPARENT
+											backgroundColor += Color.TRANSPARENT
+										}
+									}
+								}
+								column("ID", InputVM::id).cellFormat {
+									graphic = textfield(itemProperty(), UUIDConverter()) {
+										isEditable = false
+										style {
+											focusColor = Color.TRANSPARENT
+											backgroundColor += Color.TRANSPARENT
+										}
+									}
+								}
+							}
+						}
+						else
+						{
+							text("Select single component to see debug properties")
+						}
+					}
+				}
+			}
+		}
+	}
 }
